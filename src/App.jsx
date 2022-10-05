f7780b06acedfdae33a3532bec1f2571425c3d3b
bitmos <shravan.sagara@gmail.com>
2022-10-05 20:54:41 +0530

    Added Sponsors page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import NavbarX from "./components/Navbar/NavbarX.jsx";
 import Hero from "./components/Hero/Hero";
 import Particle from "./components/Particle/Particle";
+import Spons from "./components/Sponsor/Spns";
 
 function App() {
+
   return (
     <>
       <Particle />
       <NavbarX />
       <Hero />
+     <Spons />
     </>
   );
 }
